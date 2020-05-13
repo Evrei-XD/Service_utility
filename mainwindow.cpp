b76ac2d4c7346515024c168a387fb62b6cc6f268
Evrei-XD <rom_ser2011@mail.ru>
2020-05-13 16:15:19 +0300

    16:14 Начало проработки отображения максимальных значений

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -467,6 +467,15 @@ void MainWindow::set_stule()
                 "border: 1px solid #d5d5d5;"
                 "}"
                 );
+    ui->frame_4->setObjectName( "frame4" );
+    ui->frame_4->setStyleSheet(
+                "#frame4{"
+                "border-radius: 3px;"
+                "border-bottom-left-radius: 10px;"
+                "border-bottom-right-radius: 10px;"
+                "border: 1px solid #d5d5d5;"
+                "}"
+                );
 }
 
 QElapsedTimer timerUpdate;
